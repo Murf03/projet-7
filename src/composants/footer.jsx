f1f30f2de4bf8fd04f8da14001a178a67fea2f3e
Murphy Mbani <marteliembani@gmail.com>
2023-02-22 02:42:29 +0100

    Animations et backToTop

--- src/composants/footer.jsx
+++ src/composants/footer.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import '../css/composants.css';
-import { ReactComponent as Logo } from '../images/logo.svg';
+import { Link } from "react-router-dom";
+
+import "../css/composants.css";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 export default function Footer(props) {
-    let logoViewBox = "0 0 210 70";
-    return <div className='footer'>
-        <Logo viewBox={logoViewBox} fill='white' width={120} height={40} stroke='white' />
-        <p>© 2020 Kasa. All rights reserved</p>
+  let logoViewBox = "0 0 210 70";
+  return (
+    <div className="footer">
+      <Link
+        className="footer_logo"
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+        to={"#"}
+      >
+        <Logo
+          viewBox={logoViewBox}
+          fill="white"
+          width={120}
+          height={40}
+          stroke="white"
+        />
+      </Link>
+      <p>© 2020 Kasa. All rights reserved</p>
     </div>
+  );
 }
